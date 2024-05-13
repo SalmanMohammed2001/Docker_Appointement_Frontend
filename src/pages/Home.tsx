98131a0232036b69127460de10dde15963695135
SalmanMohammed2001 <salmannisthar123@gmail.com>
2024-05-14 01:22:07 +0530

    implementation Home hero left section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,51 @@
-
 const Home = () => {
-        return(
-            <div >
-                salman
-            </div>
-        )
+    return (
+        <div>
+            {/*==============hero section=============================*/}
+            <section className={"hero_section  pt-[60px] 2xl:h-[800px]"}>
+                <div className="container">
+                    <div className={"flex   flex-col lg:flex-row gap-[60px] items-center justify-between "}>
+                        <div>
+                            <div className={" lg:w-[570px] "}>
+                                <h1 className={"text-[36px] leading-[66px]  text-headingColor font-[800] md:text-[60px]"}>We
+                                    Help patient live a healthy longer life</h1>
+
+                                <p className={"text_para"}>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur excepturi
+                                    omnis qui, quidem sapiente sunt suscipit. Animi architecto blanditiis earum explicabo
+                                    fugiat harum impedit ipsa, magni neque perspiciatis sunt voluptatum.
+                                </p>
+                                <button className={"btn"}>Request Appointment</button>
+                            </div>
+
+                            <div
+                                className={"mt-[30px] lg:mt-[70px]   flex items-start flex-col lg:flex-row lg: lg:items-center gap-5 lg:gap-[30px]"}>
+                                <div>
+                                    <h2 className={"text-[36px] leading-[56px] md:text-[44px] lg:leading-[54px] font-[700] text-headingColor"}>30+</h2>
+                                    <span
+                                        className={"w-[100px] h-2 bg-yellowColor rounded-full block  mt-[5px]"}></span>
+                                    <p className={"text_para"}>Year Of Experience</p>
+                                </div>
+                                <div>
+                                    <h2 className={"text-[36px] leading-[56px] md:text-[44px] lg:leading-[54px] font-[700] text-headingColor"}>30+</h2>
+                                    <span
+                                        className={"w-[100px] h-2 bg-purpleColor rounded-full block  mt-[5px]"}></span>
+                                    <p className={"text_para"}>Clinic Location</p>
+                                </div>
+                                <div>
+                                    <h2 className={"text-[36px] leading-[56px] md:text-[44px] lg:leading-[54px] font-[700] text-headingColor"}>30+</h2>
+                                    <span
+                                        className={"w-[100px] h-2 bg-irisBlueColor rounded-full block  mt-[5px]"}></span>
+                                    <p className={"text_para"}>Patient Satisfaction</p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </div>
+    )
 }
 export default Home;
